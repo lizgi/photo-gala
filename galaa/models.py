43e94d5e7200d2533697eaa024aed7a9aacd55b4
lizgi <elizabeth.gikonyo@student.moringaschool.com>
2021-11-27 22:16:16 +0300

    add methods

--- galaa/models.py
+++ galaa/models.py
@@ -9,6 +9,25 @@ class photos(models.Model):
     image = CloudinaryField('image')
     image_description = models.CharField(max_length=100)
 
+
+    def save_photos(self):
+        self.save()
+
+
+    # update image
+    def update_photos(self, name, description):
+        self.name = name
+        self.description = description
+        # self.location = location
+        self.category = category
+        self.save()
+
+    # get all images
+    @classmethod
+    def get_all_images(cls):
+        photo = photos.objects.all()
+        return photos
+
     @classmethod
     def search_by_name(cls,search_term):
         photos = cls.objects.filter(name__icontains=search_term)
