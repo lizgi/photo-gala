08d3ad1cd927e113c8372c8b8535a66585fa4361
lizgi <elizabeth.gikonyo@student.moringaschool.com>
2021-11-28 00:48:03 +0300

    class location

--- galaa/admin.py
+++ galaa/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Category, photos
+from .models import Category, photos,Location
 
 admin.site.register(photos)
 admin.site.register(Category)
+admin.site.register(Location)
 # Register your models here.
--- galaa/models.py
+++ galaa/models.py
@@ -14,23 +14,33 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
-        
+class Location(models.Model):
+    value = models.CharField(max_length=30)
+    
+
+    def save_location(self):
+        self.save()
+
+    def __str__(self):
+        return self.value
+
 
 class photos(models.Model):
     name = models.CharField(max_length=30)
     image = CloudinaryField('image')
     image_description = models.CharField(max_length=100)
     category = models.ForeignKey('Category',on_delete=models.CASCADE, null=True)
+    location = models.ForeignKey('Location',on_delete=models.CASCADE, null=True)
 
     def save_photos(self):
         self.save()
 
     # update image
-    def update_photos(self, name, description, category):
+    def update_photos(self, name, description, category,location):
         self.name = name
         self.category = category
         self.description = description
-        # self.location = location
+        self.location = location
     
         self.save()
 
